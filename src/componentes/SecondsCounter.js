0899222512b126e0193569bd64482a923634351d
Bárbara Ulloa <ulloaby@gmail.com>
2021-04-01 10:37:57 -0300

    terminado el contador

--- src/componentes/SecondsCounter.js
+++ src/componentes/SecondsCounter.js
@@ -1,20 +1,29 @@
 import React from "react";
+import propTypes from "prop-types";
 import "./SecondsCounter.css";
 
 function SecondsCounter(props) {
   return (
     <div className="cajaGrande">
-      <div>
+      <div className="icono">
         <i className="far fa-clock"></i>
       </div>
-      <div>0</div>
-      <div>0</div>
-      <div>0</div>
-      <div>0</div>
-      <div>0</div>
-      <div>{props.segundos}</div>
+      <div className="six">{props.digitSix % 10}</div>
+      <div className="five">{props.digitFive % 10}</div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
     </div>
   );
 }
 
+SecondsCounter.propTypes = {
+  digitOne: propTypes.number,
+  digitTwo: propTypes.number,
+  digitThree: propTypes.number,
+  digitFour: propTypes.number,
+  digitFive: propTypes.number,
+  digitSix: propTypes.number,
+};
 export default SecondsCounter;
--- src/index.js
+++ src/index.js
@@ -8,13 +8,29 @@ import SecondsCounter from "./componentes/SecondsCounter";
 let segundosContador = 0;
 
 setInterval(() => {
+  const six = Math.floor(segundosContador / 100000);
+  const five = Math.floor(segundosContador / 10000);
+  const four = Math.floor(segundosContador / 1000);
+  const three = Math.floor(segundosContador / 100);
+  const two = Math.floor(segundosContador / 10);
+  const one = Math.floor(segundosContador / 1);
+  console.log(six, five, four, three, two, one);
+
   segundosContador++;
 
   ReactDOM.render(
-    <SecondsCounter segundos={segundosContador} />,
+    <SecondsCounter
+      digitOne={one}
+      digitTwo={two}
+      digitThree={three}
+      digitFour={four}
+      digitFive={five}
+      digitSix={six}
+    />,
     document.getElementById("root")
   );
 }, 1000);
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
